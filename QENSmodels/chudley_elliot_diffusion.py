24f1c191ba5a54eef37ffd76c2f3b36714c77262
Gonzalez Miguel Angel <gonzalezm@ill.fr>
2019-02-26 11:31:19 +0100

    Added two (still incomplete) models

---
+++ QENSmodels/chudley_elliot_diffusion.py
@@ -0,0 +1,152 @@
+from __future__ import print_function
+import numpy as np
+
+try:
+    import QENSmodels
+except ImportError:
+    print('Module QENSmodels not found')
+
+
+def hwhmChudleyElliotDiffusion(q, D=0.23, L=1.0):
+    """ Returns some characteristics of `ChudleyElliotDiffusion`
+
+    Parameters
+    ----------
+
+    q: float, list or :class:`~numpy:numpy.ndarray`
+        momentum transfer (in 1/Angstrom)
+
+    D: float
+        diffusion coefficient (in Angstrom^2/ps). Default to 0.23.
+
+    L: float
+        jump length (in Angstrom). Default to 1.0.
+
+
+    Returns
+    -------
+
+    hwhm: :class:`~numpy:numpy.ndarray`
+        half-width half maximum
+
+    eisf: :class:`~numpy:numpy.ndarray`
+        elastic incoherent structure factor
+
+    qisf: :class:`~numpy:numpy.ndarray`
+        quasi-elastic incoherent structure factor
+
+
+    Examples
+    --------
+
+    Notes
+    -----
+
+    """
+    # Input validation
+    q = np.asarray(q, dtype=np.float32)
+
+    eisf = np.zeros(q.size)
+    qisf = np.ones(q.size)
+    hwhm = 6 * D * (1-np.sin(q*L)/(q*L)) / L**2 
+    # Force hwhm to be numpy array, even if single value
+    hwhm = np.asarray(hwhm, dtype=np.float32)
+    hwhm = np.reshape(hwhm, hwhm.size)
+    return hwhm, eisf, qisf
+
+
+def sqwChudleyElliotDiffusion(w, q, scale=1, center=0, D=0.23,
+                                  L=1.00):
+    r""" Lorentzian model with half width half maximum equal to
+    :math:`\frac{6D}{l^2}(1 - \frac{sin(Ql)}{Ql})`
+
+    Parameters
+    ----------
+
+    w: float, list or :class:`~numpy:numpy.ndarray`
+        energy transfer (in ps)
+
+    q: float, list or :class:`~numpy:numpy.ndarray`
+        momentum transfer (non-fitting, in 1/Angstrom).
+
+    scale: float
+        scale factor. Default to 1.
+
+    center: float
+        center of peak. Default to 0.
+
+    D: float
+        diffusion coefficient (in Angstrom^2/ps). Default to 0.23.
+
+    L: float
+        jump distance (in Angstrom). Default to 1.0.
+
+    Return
+    ------
+
+    :class:`~numpy:numpy.ndarray`
+        output array
+
+
+    Examples
+    --------
+
+
+    Notes
+    -----
+
+    * The `sqwChudleyElliotDiffusion` is expressed as
+
+      .. math::
+
+          S(q, \omega) = \text{Lorentzian}(\omega, \text{scale}, \text{center},
+          \frac{6D}{l^2}(1 - \frac{sin(Ql)}{Ql}))
+
+    * Note that an equivalent expression is
+
+      .. math::
+
+          S(q, \omega) = \text{Lorentzian}(\omega, \text{scale}, \text{center},
+          \frac{1}{\tau}(1 - \frac{sin(Ql)}{Ql}))
+
+      with 
+
+      .. math::
+
+          \tau = \frac{l^2}{6D}
+
+
+
+    Reference
+    ----------
+
+    R. Hempelmann, 
+    Quasielastic Neutron Scattering and Solid State Diffusion (Oxford, 2000)
+
+    """
+    # Input validation
+    w = np.asarray(w)  # , dtype=np.float32)
+
+    q = np.asarray(q, dtype=np.float32)
+
+    # Create output array
+    sqw = np.zeros((q.size, w.size))
+
+    # Get widths, EISFs and QISFs of model
+    hwhm, eisf, qisf = hwhmChudleyElliotDiffusion(q, D, L)
+
+    # Model
+    for i in range(q.size):
+        sqw[i, :] = QENSmodels.lorentzian(w, scale, center, hwhm[i])
+
+    # For Bumps use (needed for final plotting)
+    # Using a 'Curve' in bumps for each Q --> needs vector array
+    if q.size == 1:
+        sqw = np.reshape(sqw, w.size)
+
+    return sqw
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ QENSmodels/equivalent_sites_circle.py
@@ -0,0 +1,158 @@
+from __future__ import print_function
+import numpy as np
+from scipy.special import jn
+
+try:
+    import QENSmodels
+except ImportError:
+    print('Module QENSmodels not found')
+
+
+def hwhmEquivalentSitesCircle(q, N=3, radius=1.0, resTime=1.0):
+    """
+    Returns some characteristics of `EquivalentSitesCircle`
+
+    Parameters
+    ----------
+    q: float, list or :class:`~numpy:numpy.ndarray`
+        momentum transfer (non-fitting, in 1/Angstrom)
+
+    N: integer
+        number of sites in circle. Default to 3.
+
+    radius: float
+        radius of the circle (in Angstrom). Default to 1.
+
+    resTime: float
+        residence time (in NEED TO CHECK UNITS). Default to 1.
+
+    Returns
+    -------
+    hwhm: :class:`~numpy:numpy.ndarray`
+       half-width half maximum
+
+    eisf: :class:`~numpy:numpy.ndarray`
+       elastic incoherent structure factor
+
+    qisf: :class:`~numpy:numpy.ndarray`
+       quasi-elastic incoherent structure factor
+
+
+    Examples
+    --------
+
+    """
+    # input validation
+    q = np.asarray(q, dtype=np.float32)
+
+    # Need to decide what to do if N < 2: Get out immediately with message that N_min = 2?
+    # Set N = 2 and show warning that N_min = 2?
+
+    # number of sites has to be an integer
+    N = np.int(N)
+
+    # index of sites in circle
+    sites = np.arange(N)
+
+    hwhm = 2.0 * resTime * np.sin(sites*np.pi/N)**2
+    hwhm = np.tile(hwhm, (q.size, 1))
+
+    # jump distances between sites
+    jump_distance = 2.0 * radius * np.sin(sites*np.pi/N)
+
+    # QR matrix [q.size, N] and corresponding spherical Bessel functions
+    QR = np.outer(q, jump_distance)
+    sphBessel = np.ones(QR.shape)
+    idx = np.nonzero(QR)
+    sphBessel[idx] = np.sin(QR[idx]) / QR[idx]
+
+    isf = np.zeros(QR.shape)
+    for i in range(N):
+        for j in range(N):
+            isf[:,i] += sphBessel[:,j] * np.cos(2*i*j*np.pi/N)
+        isf[:,i] /= N
+
+    eisf = isf[:,0]
+    qisf = isf[:, 1:]
+
+    return hwhm, eisf, qisf
+
+
+def sqwEquivalentSitesCircle(w, q, scale=1.0, center=0.0, N=3, radius=1.0,
+                                    resTime=1.0):
+    r"""
+    Model `Jumps between N equivalent sites on a circle` = A_0 delta + Sum of Lorentzians ...
+
+
+    Parameters
+    ----------
+
+    w: list or :class:`~numpy:numpy.ndarray`
+        energy transfer (in ps)
+
+    q: float, list or :class:`~numpy:numpy.ndarray`
+        momentum transfer (non-fitting, in 1/Angstrom)
+
+    scale: float
+        scale factor. Default to 1.
+
+    center: float
+        center of peak. Default to 0.
+
+    N: integer
+        number of sites in circle. Default to 3.
+
+    radius: float
+        radius of rotation (in Angstrom). Default to 1.
+
+    resTime: float
+        residence time in a site before jumping to another site (in 1/ps). Default to 1.
+
+    Return
+    ------
+    :class:`~numpy:numpy.ndarray`
+        output array
+
+    Examples
+    --------
+
+
+    Notes
+    -----
+
+    """
+
+    # Input validation
+    w = np.asarray(w)
+
+    q = np.asarray(q, dtype=np.float32)
+
+    # Create output array
+    sqw = np.zeros((q.size, w.size))
+
+    # Get widths, EISFs and QISFs of model
+    hwhm, eisf, qisf = hwhmEquivalentSitesCircle(q, N, radius, resTime)
+
+    # Number of Lorentzians (= N-1)
+    numberLorentz = hwhm.shape[1] - 1
+
+    # Sum of Lorentzians 
+    # (Note that hwhm has dimensions [q.size, N], as hwhm[:,0] contains a width=0, 
+    #  corresponding to the elastic line (eisf), while qisf has dimensions [q.size, N-1])
+    for i in range(q.size):
+        sqw[i, :] = eisf[i] * QENSmodels.delta(w, scale, center)
+        for j in range(numberLorentz):
+            sqw[i, :] += qisf[i, j] * QENSmodels.lorentzian(w, scale, center,
+                                                            hwhm[i, j+1])
+
+    # For Bumps use (needed for final plotting)
+    # Using a 'Curve' in bumps for each Q --> needs vector array
+    if q.size == 1:
+        sqw = np.reshape(sqw, w.size)
+
+    return sqw
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
