7db33888fec2b3f77de0ee85e6d9d20c9d55540d
celinedurniak <celine.durniak@esss.se>
2019-05-05 18:14:31 +0200

    Added settings in conf.py for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -63,6 +63,10 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+napoleon_google_docstring = False
+napoleon_use_param = False
+napoleon_use_ivar = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
