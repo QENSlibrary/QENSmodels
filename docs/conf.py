d9044e1bb10860439667bd6e81237fca0d7f9ee9
celinedurniak <celine.durniak@esss.se>
2019-05-05 19:08:45 +0200

    Removed requirements for version in conf.py for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ release = '0.1.0'  # QENSmodels.__version__  # '0.1.0'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.8'
+# needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
