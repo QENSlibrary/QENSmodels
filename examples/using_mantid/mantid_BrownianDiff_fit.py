1f0add6c9364cb81b8ed73a706b0d50458a0bf14
Celine Durniak <celine.durniak@esss.se>
2020-06-12 16:22:34 +0200

    Edited Mantid script to fix flake8 errors

--- examples/using_mantid/mantid_BrownianDiff_fit.py
+++ examples/using_mantid/mantid_BrownianDiff_fit.py
@@ -27,7 +27,9 @@ brownian_diff_noisy = QENSmodels.sqwBrownianTranslationalDiffusion(hw, Q,
                       (1 + 0.1 * added_noise) + 0.01 * added_noise
 
 # store in mantid workspace
-QENS_data = mapi.CreateWorkspace(DataX=hw, DataY=brownian_diff_noisy, NSpec=selected_wi)
+QENS_data = mapi.CreateWorkspace(DataX=hw,
+                                 DataY=brownian_diff_noisy,
+                                 NSpec=selected_wi)
 
 
 # wrap to create mantid fitting function
@@ -51,7 +53,10 @@ class my_sqwBrownian_Diffusion(mapi.IFunction1D):
 
         q = self.getAttributeValue("Q")
 
-        return QENSmodels.sqwBrownianTranslationalDiffusion(xvals, q, scale=scale, center=center,
+        return QENSmodels.sqwBrownianTranslationalDiffusion(xvals,
+                                                            q,
+                                                            scale=scale,
+                                                            center=center,
                                                             D=D)
 
 
@@ -63,22 +68,25 @@ mapi.FunctionFactory.subscribe(my_sqwBrownian_Diffusion)
 """
 
 """ Below is the template string to create individual model for each spectrum.
- 
-    A similar string can be obtained after defining the fitting model on Mantid Workbench fit 
-    wizard: 
 
-    "Setup" --> "Manage Settup" --> "Copy to Clipboard". This action will save the model
-    as a string which you can later paste onto this script.
+    A similar string can be obtained after defining the fitting model on Mantid
+    Workbench fit  wizard:
 
-    Our initial guesses are scale=7., center= 0. and D=2. 
+    "Setup" --> "Manage Settup" --> "Copy to Clipboard". This action will save
+    the model as a string which you can later paste onto this script.
+
+    Our initial guesses are scale=7., center= 0. and D=2.
 """
 
-single_model_template = """name=my_sqwBrownian_Diffusion,$domains=i,scale=7.,center=0.,D=2.,Q=_Q_"""
+single_model_template = \
+    """name=my_sqwBrownian_Diffusion,
+    $domains=i,scale=7.,center=0.,D=2.,Q=_Q_"""
 
 # Create the string representation of the global model for all spectra:
 global_model = "composite=MultiDomainFunction,NumDeriv=true;"
 for wi in range(selected_wi):
-    single_model = single_model_template.replace("_Q_", str(Q[wi]))  # insert Q-value
+    # insert Q-value
+    single_model = single_model_template.replace("_Q_", str(Q[wi]))
     global_model += f'{single_model};'
 
 # Add ties
@@ -88,18 +96,22 @@ global_model += 'ties=(f0.D=f1.D=f2.D=f3.D);constraints=(f0.D>0)'
 domain_model = dict()
 for wi in range(selected_wi):
     if wi == 0:
-        domain_model.update({"InputWorkspace": 'QENS_data', "WorkspaceIndex": str(wi),
-                             "StartX": str(-4.95), "EndX": str(4.95)})
+        domain_model.update({"InputWorkspace": 'QENS_data',
+                             "WorkspaceIndex": str(wi),
+                             "StartX": str(-4.95),
+                             "EndX": str(4.95)})
     else:
         domain_model.update(
-            {"InputWorkspace_" + str(wi): 'QENS_data', "WorkspaceIndex_" + str(wi): str(wi),
-             "StartX_" + str(wi): str(minE), "EndX_" + str(wi): str(maxE)})
+            {"InputWorkspace_" + str(wi): 'QENS_data',
+             "WorkspaceIndex_" + str(wi): str(wi),
+             "StartX_" + str(wi): str(minE),
+             "EndX_" + str(wi): str(maxE)})
 
 # Perform fitting
 mapi.Fit(Function=global_model, **domain_model,
          CreateOutput=True, MaxIteractions=500, Output='fit')
 
-"""   
+"""
  As a result of the fit, three workspaces are created:
  'fit'+"_Parameters" : optimized parameters and Chi-square
  'fit'+"_NormalisedCovarianceMatrix" : correlations between parameters
@@ -111,21 +123,25 @@ paramTable = mapi.mtd['fit_Parameters']
 # print results
 for i in range(4):
     print(f'Workspace {i}:')
-    print(f'scale: {paramTable.column(1)[3 * i]:.2f}+/-{paramTable.column(2)[3 * i]:.2e}')
-    print(f'center: {paramTable.column(1)[3 * i + 1]:.2f}+/-{paramTable.column(2)[3 * i + 1]:.2e}')
-    print(f'D: {paramTable.column(1)[3 * i + 2]:.2f}+/-{paramTable.column(2)[3 * i + 2]:.2e}')
+    print(f'scale: {paramTable.column(1)[3 * i]:.2f}')
+    print(f'center: {paramTable.column(1)[3 * i + 1]:.2f}')
+    print(f'D: {paramTable.column(1)[3 * i + 2]:.2f}')
 
 # plot results
 fig, ax = plt.subplots(2, 2)
 
-indices_plot = [(0, 0), (0, 1), (1, 0), (1, 1)]
+indx_plot = [(0, 0), (0, 1), (1, 0), (1, 1)]
 for indx, item in enumerate(mapi.mtd['fit_Workspaces']):
-    ax[indices_plot[indx][0], indices_plot[indx][1]].grid()
-    ax[indices_plot[indx][0], indices_plot[indx][1]].plot(item.readX(0), item.readY(0), label='exp')
-    ax[indices_plot[indx][0], indices_plot[indx][1]].plot(item.readX(1), item.readY(1),
-                                                          label='calc')
-    ax[indices_plot[indx][0], indices_plot[indx][1]].plot(item.readX(2), item.readY(2),
-                                                          label='diff')
+    ax[indx_plot[indx][0], indx_plot[indx][1]].grid()
+    ax[indx_plot[indx][0], indx_plot[indx][1]].plot(item.readX(0),
+                                                    item.readY(0),
+                                                    label='exp')
+    ax[indx_plot[indx][0], indx_plot[indx][1]].plot(item.readX(1),
+                                                    item.readY(1),
+                                                    label='calc')
+    ax[indx_plot[indx][0], indx_plot[indx][1]].plot(item.readX(2),
+                                                    item.readY(2),
+                                                    label='diff')
 
-ax[indices_plot[indx][0], indices_plot[indx][1]].legend()
+ax[indx_plot[indx][0], indx_plot[indx][1]].legend()
 fig.show()
