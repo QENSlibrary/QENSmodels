dccc58979013caf0827e8080e3f51955080ab578
celinedurniak <celine.durniak@esss.se>
2022-04-03 21:36:05 +0200

    Add github actions

--- setup.py
+++ setup.py
@@ -7,14 +7,17 @@ authors = ', '.join(author_list[:-1]) + ', and ' + author_list[-1]
 
 short = 'Library of models for fitting QENS data'
 
+packages = ['QENSmodels']
+
 setup(name='QENSmodels',
-      version='0.1.1',
+      version='0.1.2',
       description=short,
       url='https://github.com/QENSlibrary/QENSmodels',
       author=authors,
       author_email='  ',
       license=open('LICENSE.txt').read(),
-      packages=find_packages(),
+      python_requires=">=3.7,<3.11",
+      packages=packages,
       install_requires=['scipy', 'numpy', ],
       setup_requires=['flake8'],
       zip_safe=False,)
